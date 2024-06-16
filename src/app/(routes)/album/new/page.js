5e2b35ed7fee86297306374f8d9708625fdc78a6
Anis Romzi <anis000romzi@gmail.com>
2024-06-16 11:45:27 +0700

    add album input component

--- src/app/(routes)/album/new/page.js
+++ src/app/(routes)/album/new/page.js
@@ -1,47 +1,21 @@
 'use client';
-// hooks
-import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useRouter } from 'next/navigation';
-import useInput from '@/app/_hooks/useInput';
-
-// components
-import Image from 'next/image';
-
-// utils
+import AlbumInput from '@/app/_components/inputs/AlbumInput';
 import api from '@/app/_utils/api';
-
-// icons 
-import { FaPen } from 'react-icons/fa6';
-
-// styles
-import inputStyles from '../../../_styles/input.module.css';
 import styles from '../../../_styles/style.module.css';
-
-// assets
-import defaultImage from '../../../_assets/default-image.png';
 
 function NewAlbum() {
   const router = useRouter();
 
   const authUser = useSelector((state) => state.authUser);
 
-  const [name, onNameChange] = useInput('');
-  const [year, onYearChange] = useInput('');
-  const [file, setFile] = useState(null);
-
   if (!authUser || !authUser.is_active) {
     router.push('/');
     return null;
   }
 
-  const handleFileChange = (event) => {
-    if (event.target.files) {
-      setFile(event.target.files[0]);
-    }
-  };
-
-  const addAlbum = async () => {
+  const addAlbum = async (name, year, file) => {
     try {
       if (file && file.size > 512000) {
         throw new Error('File is too big');
@@ -61,45 +35,7 @@ function NewAlbum() {
   return (
     <main className={styles.new_album_page}>
       <h1>New Album</h1>
-      <form className={inputStyles.new_album_input}>
-        <input
-          type="file"
-          id="cover"
-          name="cover"
-          style={{ display: 'none' }}
-          onChange={handleFileChange}
-        />
-        <label htmlFor="cover" className={inputStyles.container}>
-          <Image
-            src={file ? URL.createObjectURL(file) : defaultImage}
-            width={200}
-            height={200}
-            alt="Album cover"
-          />
-          <div className={inputStyles.overlay}>
-            <div className={inputStyles.text}>
-              <FaPen />
-            </div>
-          </div>
-        </label>
-        <div>
-          <input
-            type="text"
-            value={name}
-            onChange={onNameChange}
-            placeholder="Album name"
-          />
-          <input
-            type="number"
-            value={year}
-            onChange={onYearChange}
-            placeholder="Year"
-          />
-          <button type="button" onClick={addAlbum}>
-            Create Album
-          </button>
-        </div>
-      </form>
+      <AlbumInput addAlbum={addAlbum} />
     </main>
   );
 }
---
+++ src/app/_components/inputs/AlbumInput.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import useInput from '../../_hooks/useInput';
+import Image from 'next/image';
+import { FaPen } from 'react-icons/fa6';
+import styles from '../../_styles/input.module.css';
+import defaultImage from '../../_assets/default-image.png';
+
+function AlbumInput({ addAlbum }) {
+  const [name, onNameChange] = useInput('');
+  const [year, onYearChange] = useInput('');
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    if (event.target.files) {
+      setFile(event.target.files[0]);
+    }
+  };
+
+  return (
+    <form className={styles.new_album_input}>
+      <input
+        type="file"
+        id="cover"
+        name="cover"
+        style={{ display: 'none' }}
+        onChange={handleFileChange}
+      />
+      <label htmlFor="cover" className={styles.container}>
+        <Image
+          src={file ? URL.createObjectURL(file) : defaultImage}
+          width={200}
+          height={200}
+          alt="Album cover"
+        />
+        <div className={styles.overlay}>
+          <div className={styles.text}>
+            <FaPen />
+          </div>
+        </div>
+      </label>
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={onNameChange}
+          placeholder="Album name"
+        />
+        <input
+          type="number"
+          value={year}
+          onChange={onYearChange}
+          placeholder="Year"
+        />
+        <button type="button" onClick={() => addAlbum(name, year, file)}>
+          Create Album
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default AlbumInput;
