d4339480f70a4246d1e2000bd91451aa1dcda9a0
Anis Romzi <anis000romzi@gmail.com>
2024-06-16 11:45:59 +0700

    minor fix

--- src/app/(routes)/register/page.js
+++ src/app/(routes)/register/page.js
@@ -1,6 +1,5 @@
 'use client';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 import { useDispatch } from 'react-redux';
 import { asyncSetAuthUser } from '@/app/_states/authUser/action';
 import api from '@/app/_utils/api';
@@ -8,14 +7,12 @@ import RegisterInput from '@/app/_components/inputs/RegisterInput';
 import styles from '../../_styles/style.module.css';
 
 function Register() {
-  const router = useRouter();
   const dispatch = useDispatch();
 
   const onRegister = async ({ email, username, fullname, password }) => {
     try {
       await api.register({ email, username, fullname, password });
       dispatch(asyncSetAuthUser({ usernameOrEmail: email, password }));
-      // router.push('/login');
     } catch (error) {
       alert(error.message);
     }
