bb8ad4f0cd7d1a658429e828b7979c95073117fe
Anis Romzi <anis000romzi@gmail.com>
2024-07-15 19:14:09 +0700

    add timer on send activation code

--- src/app/(routes)/activate/page.js
+++ src/app/(routes)/activate/page.js
@@ -1,33 +1,71 @@
 'use client';
+import { useState, useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Link from 'next/link';
 import VerificationInput from '@/app/_components/inputs/VerificationInput';
-import { asyncUnsetAuthUser, asyncActivateUser } from '@/app/_states/authUser/action';
+import {
+  asyncUnsetAuthUser,
+  asyncActivateUser,
+} from '@/app/_states/authUser/action';
 import { redirect } from 'next/navigation';
+import { toast } from 'react-toastify';
 import api from '@/app/_utils/api';
+import { formatTime } from '@/app/_utils/time-format';
 import { CiLogout } from 'react-icons/ci';
 import styles from '../../_styles/style.module.css';
 
+const initialTime = 60 * 2;
+
 function Activate() {
-  const authUser = useSelector((states) => states.authUser);
+  const [timeLeft, setTimeLeft] = useState(() => {
+    const savedTime = sessionStorage.getItem('time-left');
+    return savedTime ? parseInt(savedTime, 10) : initialTime;
+  });
+  const [isSent, setIsSent] = useState(() => {
+    const sentStatus = sessionStorage.getItem('sent');
+    return !!sentStatus;
+  });
+
+  const authUser = useSelector((state) => state.authUser);
   const dispatch = useDispatch();
 
-  const sendCode = async (userId) => {
+  const sendCode = useCallback(async (userId) => {
     try {
       const message = await api.sendActivationCode(userId);
-      alert(message);
+      sessionStorage.setItem('sent', true);
+      setTimeLeft(initialTime);
+      setIsSent(true);
+      toast.info(message);
     } catch (error) {
-      alert(error.message);
+      toast.error(error.message);
     }
-  };
+  }, []);
 
-  const logOut = () => {
+  useEffect(() => {
+    sessionStorage.setItem('time-left', timeLeft);
+
+    if (isSent && timeLeft > 0) {
+      const interval = setInterval(() => {
+        setTimeLeft((prevTime) => prevTime - 1);
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+
+    if (timeLeft === 0) {
+      sessionStorage.removeItem('time-left');
+      sessionStorage.removeItem('sent');
+      setIsSent(false);
+    }
+  }, [timeLeft, isSent]);
+
+  const logOut = useCallback(() => {
     dispatch(asyncUnsetAuthUser());
-  };
+  }, [dispatch]);
 
-  const activateUser = (userId, otp) => {
+  const activateUser = useCallback((userId, otp) => {
     dispatch(asyncActivateUser(userId, otp));
-  };
+  }, [dispatch]);
 
   if (!authUser) {
     redirect('/login');
@@ -38,7 +76,10 @@ function Activate() {
   }
 
   const { id, email } = authUser;
-  const maskedEmail = email.replace(/^(\w{2})(\w+)(\w{2})(@[\w.]+)$/, '$1****$3$4');
+  const maskedEmail = email.replace(
+    /^(\w{2})(\w+)(\w{2})(@[\w.]+)$/,
+    '$1****$3$4'
+  );
 
   return (
     <main className={styles.activate_page}>
@@ -50,9 +91,23 @@ function Activate() {
         OTP code will be sent to: {maskedEmail} <Link href="/changeemail">Change email</Link>
       </p>
       <VerificationInput verify={activateUser} userId={id} />
-      <button id="send-activation-code" className={styles.activate_page__send} onClick={() => sendCode(id)}>
-        Send Code
-      </button>
+      {isSent ? (
+        <button
+          id="send-activation-code"
+          className={styles.activate_page__send}
+          onClick={() => {}}
+        >
+          {formatTime(timeLeft)}
+        </button>
+      ) : (
+        <button
+          id="send-activation-code"
+          className={styles.activate_page__send}
+          onClick={() => sendCode(id)}
+        >
+          Send Code
+        </button>
+      )}
     </main>
   );
 }
