fbc580e9ad1bb93fbf71a1d6d10f1f264eb94f5f
Anis Romzi <anis000romzi@gmail.com>
2024-07-26 21:42:48 +0700

    sortable queue

--- src/app/_components/queue/QueueItem.jsx
+++ src/app/_components/queue/QueueItem.jsx
@@ -1,9 +1,91 @@
+import { useRef, useEffect } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
 import { AiOutlineClose } from 'react-icons/ai';
 import styles from '../../_styles/queue.module.css';
 
-function QueueItem({ id, title, artist, onPlay, onDelete, currentlyPlaying }) {
+function QueueItem({
+  id,
+  title,
+  artist,
+  onPlay,
+  onDelete,
+  currentlyPlaying,
+  index,
+  moveCard,
+}) {
+  const ref = useRef(null);
+  const [{ handlerId }, drop] = useDrop({
+    accept: 'queue-item',
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      };
+    },
+    hover(item, monitor) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      const clientOffset = monitor.getClientOffset();
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+
+      moveCard(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    },
+  });
+
+  const [{ isDragging }, drag, preview] = useDrag({
+    type: 'queue-item',
+    item: () => {
+      // Add no-select class to disable text selection
+      document.body.classList.add(styles.no_select);
+
+      return { id, index, title, artist };
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+    end: () => {
+      // Remove no-select class when dragging ends
+      document.body.classList.remove(styles.no_select);
+    },
+  });
+
+  useEffect(() => {
+    // Clean up the no-select class if the component unmounts during a drag
+    return () => {
+      document.body.classList.remove(styles.no_select);
+    };
+  }, []);
+
+  const opacity = isDragging ? 0 : 1;
+  drag(drop(ref));
+
   return (
-    <div className={`${styles.queue_item} ${currentlyPlaying.id === id ? styles.playing : ''}`} onClick={() => onPlay(id)}>
+    <div
+      ref={ref}
+      style={{ opacity }}
+      className={`${styles.queue_item} ${
+        currentlyPlaying.id === id ? styles.playing : ''
+      }`}
+      onClick={() => onPlay(id)}
+      data-handler-id={handlerId}
+    >
       <div className={styles.queue_info}>
         <strong>{title}</strong>
         <p>{artist}</p>
--- src/app/_components/queue/QueueList.jsx
+++ src/app/_components/queue/QueueList.jsx
@@ -1,3 +1,6 @@
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { setQueuePosition } from '@/app/_states/queue/action';
 import QueueItem from './QueueItem';
 import styles from '../../_styles/queue.module.css';
 
@@ -7,17 +10,25 @@ function QueueList({
   onDeleteHandler,
   currentlyPlaying,
 }) {
+  const dispatch = useDispatch();
+
+  const moveCard = useCallback((dragIndex, hoverIndex) => {
+    dispatch(setQueuePosition(dragIndex, hoverIndex));
+  }, [dispatch]);
+
   return (
     <div className={styles.queue_list}>
       {queue &&
-        queue.map((queue) => (
-          <QueueItem
-            key={queue.id}
-            {...queue}
-            onPlay={onPlayHandler}
-            onDelete={onDeleteHandler}
-            currentlyPlaying={currentlyPlaying}
-          />
+        queue.map((queue, index) => (
+      <QueueItem
+        key={queue.id}
+        {...queue}
+        onPlay={onPlayHandler}
+        onDelete={onDeleteHandler}
+        currentlyPlaying={currentlyPlaying}
+        index={index}
+        moveCard={moveCard}
+      />
         ))}
     </div>
   );
